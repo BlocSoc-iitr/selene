e0d133d20ce35e50fd2a0bd2aa71000384f212ac
gerceboss <nilavprajapati2004@gmail.com>
2024-08-18 17:22:26 +0530

    added basic type

--- common/types.go
+++ common/types.go
@@ -1,7 +1,43 @@
 package common
 
-type Block struct {
+//other option is to import a package of go-ethereum but that was weird
+import (
+	"github.com/holiman/uint256"
+)
+
+// need to confirm how such primitive types will be imported,
+//
+//Transaction  https://docs.rs/alloy/latest/alloy/rpc/types/struct.Transaction.html
+// address:  20 bytes
+// B256: 32 bytes  https://bluealloy.github.io/revm/docs/revm/precompile/primitives/type.B256.html
+type Address struct{
+   addr [20]byte
 }
+
+type Block struct {
+   number uint64
+   base_fee_per_gas uint256.Int
+   difficulty uint256.Int
+   extra_data []byte
+   gas_limit uint64
+   gas_used uint64
+   hash [32]byte
+   logs_bloom []byte
+   miner Address
+   mix_hash [32]byte
+   nonce string
+   parent_hash [32]byte
+   receipts_root [32]byte
+   sha3_uncles [32]byte
+   size uint64
+   state_root [32]byte
+   timestamp uint64
+   total_difficulty uint64
+   transactions Transactions
+   transactions_root [32]byte
+   uncles [][32]byte
+}
+// an enum having 2 types- how to implement??
 type Transactions struct {
 }
 
@@ -10,4 +46,4 @@ type BlockTag struct {
 }
 
 // need some error structs and enums as well
-// Example: BlockNotFoundError
+// Example BlockNotFoundError
--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/bits-and-blooms/bitset v1.7.0 // indirect
 	github.com/consensys/bavard v0.1.13 // indirect
 	github.com/consensys/gnark-crypto v0.12.1 // indirect
+	github.com/holiman/uint256 v1.3.1 // indirect
 	github.com/mmcloughlin/addchain v0.4.0 // indirect
 	github.com/wealdtech/go-merkletree v1.0.0 // indirect
 	golang.org/x/crypto v0.10.0 // indirect
