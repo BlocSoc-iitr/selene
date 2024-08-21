70392e3c614b066e47a8782297bbff2e34dc4b2a
18aaddy <aaddyrocks123@gmail.com>
2024-08-21 19:51:36 +0530

    Added common/utils.go

--- common/utils.go
+++ common/utils.go
@@ -1,19 +1,43 @@
 package common
 
+import (
+	"encoding/hex"
+	"encoding/json"
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/common"
+)
+
 // if we need to export the functions , just make their first letter capitalised
-func hex_str_to_bytes() {
+func Hex_str_to_bytes(s string) ([]byte){
+	bytesArray, _ := hex.DecodeString(s[2:])
+	return bytesArray
 }
 
-func address_to_hex_string() {
+func Address_to_hex_string(addr common.Address) (string){
+	bytesArray := addr.Bytes()
+	return fmt.Sprintf("0x%x", hex.EncodeToString(bytesArray))
 }
 
-func u64_to_hex_string() {
+func U64_to_hex_string(val uint64) (string){
+	return fmt.Sprintf("0x%x", val)
 }
 
-func bytes_deserialize() {
+func Bytes_deserialize(data []byte) ([]byte, error) {
+    var hexString string
+    if err := json.Unmarshal(data, &hexString); err != nil {
+        return nil, err
+    }
+    
+    bytes, err := hex.DecodeString(hexString)
+    if err != nil {
+        return nil, err
+    }
 
+    return bytes, nil
 }
 
-func bytes_serialize() {
-
+func Bytes_serialize(bytes []byte) ([]byte, error) {
+    hexString := hex.EncodeToString(bytes)
+    return json.Marshal(hexString)
 }
