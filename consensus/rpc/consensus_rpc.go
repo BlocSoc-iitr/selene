9e38ef161dd2694065efaff23fd580a6029e6755
Sambhav Jain <136801346+DarkLord017@users.noreply.github.com>
2024-09-20 07:44:02 +0530

    Update consensus_rpc.go to remove errors

--- consensus/rpc/consensus_rpc.go
+++ consensus/rpc/consensus_rpc.go
@@ -6,11 +6,11 @@ import (
 
 // return types not mention and oarameters as well
 type ConsensusRpc interface {
-	GetBootstrap(block_root []byte) (consensus_core.Bootstrap, error)
+	GetBootstrap(block_root [32]byte) (consensus_core.Bootstrap, error)
 	GetUpdates(period uint64, count uint8) ([]consensus_core.Update, error)
 	GetFinalityUpdate() (consensus_core.FinalityUpdate, error)
 	GetOptimisticUpdate() (consensus_core.OptimisticUpdate, error)
-	GetBlock(slot uint64) (consensus_core.BeaconBlock, error)
+	GetBlock(slot uint64) (*consensus_core.BeaconBlock, error)
 	ChainId() (uint64, error)
 }
 
