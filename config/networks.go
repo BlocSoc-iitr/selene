45efb384610cd9a7337a15b26b86f5366bd977cb
DarkLord017 <sambhavjain170944@gmail.com>
2024-09-21 22:46:40 +0530

    Code in config directory uses utils package

--- config/networks.go
+++ config/networks.go
@@ -1,18 +1,23 @@
 package config
+
 import (
 	"fmt"
-	"github.com/BlocSoc-iitr/selene/common"
-	"github.com/pkg/errors"
 	"os"
 	"path/filepath"
 	"strings"
+
+	"github.com/BlocSoc-iitr/selene/utils"
+	"github.com/pkg/errors"
 )
+
 type Network string
+
 const (
 	MAINNET Network = "MAINNET"
 	GOERLI  Network = "GOERLI"
 	SEPOLIA Network = "SEPOLIA"
 )
+
 func (n Network) BaseConfig(s string) (BaseConfig, error) {
 	switch strings.ToUpper(s) {
 	case "MAINNET":
@@ -52,7 +57,7 @@ func (n Network) ChainID(id uint64) (BaseConfig, error) {
 		}
 		return config, nil
 	case 11155111:
-		
+
 		config, err := Sepolia()
 		if err != nil {
 			return BaseConfig{}, err
@@ -63,19 +68,19 @@ func (n Network) ChainID(id uint64) (BaseConfig, error) {
 	}
 }
 func dataDir(network Network) (string, error) {
-    homeDir, err := os.UserHomeDir()
-    if err != nil {
-        return "", fmt.Errorf("failed to get user home directory: %w", err)
-    }
-    path := filepath.Join(homeDir, fmt.Sprintf("selene/data/%s", strings.ToLower(string(network))))
-    return path, nil
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("failed to get user home directory: %w", err)
+	}
+	path := filepath.Join(homeDir, fmt.Sprintf("selene/data/%s", strings.ToLower(string(network))))
+	return path, nil
 }
 func Mainnet() (BaseConfig, error) {
-	defaultCheckpoint, err := common.Hex_str_to_bytes("c7fc7b2f4b548bfc9305fa80bc1865ddc6eea4557f0a80507af5dc34db7bd9ce")
+	defaultCheckpoint, err := utils.Hex_str_to_bytes("c7fc7b2f4b548bfc9305fa80bc1865ddc6eea4557f0a80507af5dc34db7bd9ce")
 	if err != nil {
 		return BaseConfig{}, fmt.Errorf("failed to parse default checkpoint: %w", err)
 	}
-	genesisRoot, err := common.Hex_str_to_bytes("4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95")
+	genesisRoot, err := utils.Hex_str_to_bytes("4b363db94e286120d76eb905340fdd4e54bfe9f06bf33ff6cf5ad27f511bfe95")
 	if err != nil {
 		return BaseConfig{}, fmt.Errorf("failed to parse genesis root: %w", err)
 	}
@@ -111,15 +116,15 @@ func Mainnet() (BaseConfig, error) {
 				ForkVersion: []byte{0x04, 0x00, 0x00, 0x00}},
 		},
 		MaxCheckpointAge: 1_209_600, // 14 days
-		DataDir:        &dataDir,
+		DataDir:          &dataDir,
 	}, nil
 }
 func Goerli() (BaseConfig, error) {
-	defaultCheckpoint, err := common.Hex_str_to_bytes("f6e9d5fdd7c406834e888961beab07b2443b64703c36acc1274ae1ce8bb48839")
+	defaultCheckpoint, err := utils.Hex_str_to_bytes("f6e9d5fdd7c406834e888961beab07b2443b64703c36acc1274ae1ce8bb48839")
 	if err != nil {
 		return BaseConfig{}, fmt.Errorf("failed to parse default checkpoint: %w", err)
 	}
-	genesisRoot, err := common.Hex_str_to_bytes("043db0d9a83813551ee2f33450d23797757d430911a9320530ad8a0eabc43efb")
+	genesisRoot, err := utils.Hex_str_to_bytes("043db0d9a83813551ee2f33450d23797757d430911a9320530ad8a0eabc43efb")
 	if err != nil {
 		return BaseConfig{}, fmt.Errorf("failed to parse genesis root: %w", err)
 	}
@@ -158,11 +163,11 @@ func Goerli() (BaseConfig, error) {
 	}, nil
 }
 func Sepolia() (BaseConfig, error) {
-	defaultCheckpoint, err := common.Hex_str_to_bytes("4135bf01bddcfadac11143ba911f1c7f0772fdd6b87742b0bc229887bbf62b48")
+	defaultCheckpoint, err := utils.Hex_str_to_bytes("4135bf01bddcfadac11143ba911f1c7f0772fdd6b87742b0bc229887bbf62b48")
 	if err != nil {
 		return BaseConfig{}, fmt.Errorf("failed to parse default checkpoint: %w", err)
 	}
-	genesisRoot, err := common.Hex_str_to_bytes("d8ea171f3c94aea21ebc42a1ed61052acf3f9209c00e4efbaaddac09ed9b8078")
+	genesisRoot, err := utils.Hex_str_to_bytes("d8ea171f3c94aea21ebc42a1ed61052acf3f9209c00e4efbaaddac09ed9b8078")
 	if err != nil {
 		return BaseConfig{}, fmt.Errorf("failed to parse genesis root: %w", err)
 	}
